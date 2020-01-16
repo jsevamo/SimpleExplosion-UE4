5060240ce81e692514c40b3dae84fd2dcf9ba1a4
jsevamo <j_sevamo@hotmail.com>
2020-01-16 12:20:48 -0500

    Initial commit

---
+++ Source/SimpleExplosionUE4/SimpleExplosionUE4.cpp
@@ -0,0 +1,6 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "SimpleExplosionUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SimpleExplosionUE4, "SimpleExplosionUE4" );
---
+++ Source/SimpleExplosionUE4/SimpleExplosionUE4GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleExplosionUE4GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEEXPLOSIONUE4_API ASimpleExplosionUE4GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
